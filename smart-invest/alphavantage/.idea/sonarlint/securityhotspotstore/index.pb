b7512e0b102b8c7eb3fd1cf487d56f538b560cf3
Denislav Zaimov <dzaimov@gmail.com>
2023-12-09 16:19:21 +0200

    Changing Maven structure to be multi-module project.

---
+++ smart-invest/alphavantage/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+X
+(src/main/java/org/alphavantage/Main.java,0\1\01171aed195abfe5bc268772ed0ab60fceb112de
---
+++ smart-invest/alphavantage/src/main/java/org/alphavantage/Main.java
@@ -0,0 +1,11 @@
+package org.alphavantage;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static String sayHi() {
+        return "Hi";
+    }
+}
